cce559709b01ff6304a747d2876e4c52dc44a845
Andrei Cristian Petcu <andrei@ceata.org>
2017-04-21 20:24:16 +0300

    initial commit

---
+++ background.js
@@ -0,0 +1,17 @@
+function remove_searchparams(requestDetails) {
+    const url = new URL(requestDetails.url);
+    const host = url.host;
+    if (host.includes("wikipedia.org") && !host.endsWith("m.wikipedia.org")) {
+        const new_host = host.replace("wikipedia.org", "m.wikipedia.org");
+        const mobile_url = url.protocol + "//" + new_host + url.pathname;
+        return {
+            redirectUrl: mobile_url
+        };
+    }
+}
+browser.webRequest.onBeforeRequest.addListener(
+    remove_searchparams, {
+        urls: ["*://*.wikipedia.org/*"],
+        types: ["main_frame"]
+    }, ["blocking"]
+);
