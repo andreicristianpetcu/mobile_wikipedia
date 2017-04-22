539a02ddc2821637f3847b4a9c7e1fb2da55d2cb
Andrei Cristian Petcu <andrei@ceata.org>
2017-04-22 21:23:55 +0300

    small improvements on the expand contents section

--- content.js
+++ content.js
@@ -1,9 +1,19 @@
-const colapsableBlock = document.querySelectorAll(".toc-mobile.view-border-box h2.collapsible-heading")[0];
-if (colapsableBlock) {
-    const click = new MouseEvent('click', {
+function expandContents(){
+  // alert('damn it now it works');
+  const colapsableBlock = document.querySelectorAll("h2.collapsible-heading")[0];
+  // const colapsableBlock = document.querySelectorAll(".toc-mobile.view-border-box h2.collapsible-heading")[0];
+  if (colapsableBlock) {
+    const isNotExpanded = !colapsableBlock.classList.contains("open-block");
+    if (isNotExpanded){
+      const click = new MouseEvent('click', {
         'view': window,
         'bubbles': true,
         'cancelable': true
-    });
-    colapsableBlock.dispatchEvent(click);
-}
+      });
+      colapsableBlock.dispatchEvent(click);
+    }
+  }
+};
+window.onload = function(){
+  window.setTimeout(expandContents, 500);
+};
